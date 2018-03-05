7441f15886a7ffe95d321849f976ac8b357fc5bf
Close Su <close.su@kyperdata.com>
2018-03-05 16:21:10 +0800

    add hystrix dashboard

--- comsumer/src/main/java/com/example/comsumer/controller/ConsumerController.java
+++ comsumer/src/main/java/com/example/comsumer/controller/ConsumerController.java
@@ -3,6 +3,7 @@ package com.example.comsumer.controller;
 import com.example.comsumer.entity.User;
 import com.example.comsumer.feign.UserFiegnClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
--- comsumer/src/main/java/com/example/comsumer/feign/UserFiegnClient.java
+++ comsumer/src/main/java/com/example/comsumer/feign/UserFiegnClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("provider-user")
+@FeignClient(value = "provider-user", fallback = HystrixClentFallback.class)
 public interface UserFiegnClient {
     @GetMapping("/user/{id}")
     User findById(@PathVariable("id") Long id);
